a19060ea9bd1248beba2754b833cee6b1e9b2d24
ethqnol <112029223+ethqnol@users.noreply.github.com>
2023-04-03 14:09:32 -0400

    Text File Creation/Parsing

---
+++ main.py
@@ -0,0 +1,17 @@
+import math
+
+
+
+with open("data.txt", "r") as data:
+    
+    filelines = data.readlines()
+    timestamp = []
+    value = []
+
+    for l in filelines:
+        datapoint = l.split(" ")
+        timestamp.append(datapoint[0])
+        value.append(datapoint[1].replace("\n", ""))
+
+    print(timestamp)
+    print(value)
